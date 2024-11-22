8c9de3483db0676ad3d3bc7d419ee3da307cc0d4
Ivan Ivanov <ivveni@abv.bg>
2024-11-22 22:41:24 +0200

    Added CastingAgentController with CompleteProfile Action and View

--- ActorsCastingsProject/ActorsCastings.Web.ViewModels/CastingAgentProfileViewModel.cs
+++ ActorsCastingsProject/ActorsCastings.Web.ViewModels/CastingAgentProfileViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace ActorsCastings.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActorsCastings.Web.ViewModels
 {
     public class CastingAgentProfileViewModel
     {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public string? CastingAgency { get; set; }
     }
 }
---
+++ ActorsCastingsProject/ActorsCastings.Web/Controllers/CastingAgentController.cs
@@ -0,0 +1,58 @@
+﻿using ActorsCastings.Data.Models;
+using ActorsCastings.Web.Data;
+using ActorsCastings.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActorsCastings.Web.Controllers
+{
+    public class CastingAgentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CastingAgentController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult CompleteProfile()
+        {
+            var model = new CastingAgentProfileViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CompleteProfile(CastingAgentProfileViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationUser? currentUser = await _userManager.GetUserAsync(User);
+
+            if (currentUser == null)
+            {
+                return View("Error");
+            }
+
+            var castingAgentProfile = new CastingAgentProfile()
+            {
+                Name = model.Name,
+                CastingAgency = model.CastingAgency,
+                UserId = currentUser.Id
+            };
+
+
+            currentUser.CastingAgentProfileId = castingAgentProfile.Id;
+
+            await _context.CastingAgentProfiles.AddAsync(castingAgentProfile);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
