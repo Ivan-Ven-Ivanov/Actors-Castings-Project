7e5530110a76cdf54e83687f3551851a70106afe
Ivan Ivanov <ivveni@abv.bg>
2024-11-22 14:16:13 +0200

    Scaffolded Identity pages, add Add Action in Casting Controller
    The add action still doesn't work

--- ActorsCastingsProject/ActorsCastings.Web.ViewModels/CastingViewModel.cs
+++ ActorsCastingsProject/ActorsCastings.Web.ViewModels/CastingViewModel.cs
@@ -2,14 +2,13 @@
 {
     public class CastingViewModel
     {
-        public string Id { get; set; } = null!;
-
+        public string? Id { get; set; }
         public string Title { get; set; } = null!;
 
         public string Description { get; set; } = null!;
 
         public string CastingEnd { get; set; } = null!;
 
-        public string CastingAgentId { get; set; } = null!;
+        public string? CastingAgentId { get; set; }
     }
 }
--- ActorsCastingsProject/ActorsCastings.Web/Controllers/CastingController.cs
+++ ActorsCastingsProject/ActorsCastings.Web/Controllers/CastingController.cs
@@ -1,7 +1,10 @@
-﻿using ActorsCastings.Web.Data;
+﻿using ActorsCastings.Data.Models;
+using ActorsCastings.Web.Data;
 using ActorsCastings.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using static ActorsCastings.Common.EntityValidationConstants.Casting;
 
 namespace ActorsCastings.Web.Controllers
@@ -9,12 +12,16 @@ namespace ActorsCastings.Web.Controllers
     public class CastingController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CastingController(ApplicationDbContext context)
+        public CastingController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
+
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<CastingViewModel> models = await _context.Castings
@@ -29,6 +36,49 @@ namespace ActorsCastings.Web.Controllers
                 .ToListAsync();
 
             return View(models);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new CastingViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CastingViewModel model)
+        {
+
+            ApplicationUser? user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            bool isDateCorrect = DateTime.TryParseExact(model.CastingEnd, CastingCastingEndDateTimeFormatString, CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime dateTime);
+
+            if (!isDateCorrect)
+            {
+                return View(model);
+            }
+
+            Casting casting = new Casting()
+            {
+                Title = model.Title,
+                Description = model.Description,
+                CastingEnd = dateTime,
+                CastingAgent = user.CastingAgentProfile
+            };
+
+            await _context.Castings.AddAsync(casting);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
