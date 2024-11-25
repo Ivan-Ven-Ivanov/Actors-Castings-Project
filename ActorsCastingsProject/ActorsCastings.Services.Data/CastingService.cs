c439b116e1d24f7186573acb57f3dc98407021d9
Ivan Ivanov <ivveni@abv.bg>
2024-11-25 22:24:12 +0200

    Casting Service Added

---
+++ ActorsCastingsProject/ActorsCastings.Services.Data/CastingService.cs
@@ -0,0 +1,41 @@
+﻿using ActorsCastings.Data.Models;
+using ActorsCastings.Data.Repository.Interfaces;
+using ActorsCastings.Services.Data.Interfaces;
+using ActorsCastings.Web.ViewModels.Casting;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActorsCastings.Services.Data
+{
+    public class CastingService : ICastingService
+    {
+        private readonly IRepository<Casting, Guid> _repository;
+
+        public CastingService(IRepository<Casting, Guid> repository)
+        {
+            _repository = repository;
+        }
+        public async Task AddCastingAsync(AddCastingViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CastingDetailsViewModel> GetCastingDetailsByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<CastingViewModel>> IndexGetAllAsync()
+        {
+            IEnumerable<CastingViewModel> models = await _repository.GetAllAttached()
+                .Select(c => new CastingViewModel
+                {
+                    Id = c.Id.ToString(),
+                    Title = c.Title,
+                    CastingEnd = c.CastingEnd.ToString(Common.EntityValidationConstants.Casting.CastingCastingEndDateTimeFormatString)
+                })
+                .ToListAsync();
+
+            return models;
+        }
+    }
+}
