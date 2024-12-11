124c3f91c8e1d5751d788e24bcd958c8c0e0e75b
Ivan Ivanov <ivveni@abv.bg>
2024-12-11 20:34:04 +0200

    Added unit tests for Movie and Play services

--- ActorsCastingsProject/ActorCastings.Services.Tests/PlayServiceTests.cs
+++ ActorsCastingsProject/ActorCastings.Services.Tests/PlayServiceTests.cs
@@ -2,36 +2,37 @@
 using ActorsCastings.Data.Repository.Interfaces;
 using ActorsCastings.Services.Data;
 using ActorsCastings.Services.Data.Interfaces;
-using ActorsCastings.Web.ViewModels.Movie;
+using ActorsCastings.Web.ViewModels.Play;
 using MockQueryable;
 using Moq;
 using System.Linq.Expressions;
 
 namespace ActorCastings.Services.Tests
 {
+
     [TestFixture]
-    public class MovieServiceTests
+    public class PlayServiceTests
     {
-        private Mock<IRepository<Movie, Guid>> _movieRepository;
+        private Mock<IRepository<Play, Guid>> _playRepository;
         private Mock<IRepository<Actor, Guid>> _actorRepository;
-        private Mock<IRepository<ActorMovie, Guid>> _actorMovieRepository;
+        private Mock<IRepository<ActorPlay, Guid>> _actorPlayRepository;
 
-        private IMovieService movieService;
+        private IPlayService playService;
 
-        private IList<Movie> movies = new List<Movie>()
+        private IList<Play> plays = new List<Play>()
         {
-            new Movie()
+            new Play()
             {
-                Id = Guid.Parse("395413df-fe4d-4caf-8875-c3dc050a44ed"),
-                Title = "New Movie",
-                Genre = "Action",
+                Id = Guid.Parse("2c459674-8238-4ea8-8fe4-21f0bf5acb16"),
+                Title = "New Play",
+                Theatre = "National",
                 Director = "John John",
-                Description = "Test description of the new movie",
-                ActorsMovies = new List<ActorMovie>
+                Description = "Test description of the new play",
+                ActorsPlays = new List<ActorPlay>
                 {
-                    new ActorMovie()
+                    new ActorPlay()
                     {
-                        MovieId = Guid.Parse("395413df-fe4d-4caf-8875-c3dc050a44ed"),
+                        PlayId = Guid.Parse("2c459674-8238-4ea8-8fe4-21f0bf5acb16"),
                         ActorId =Guid.Parse("e2c67618-1a8d-47da-8778-a9df40503b56"),
                         IsApproved = true,
                         Actor = new Actor()
@@ -51,13 +52,13 @@ namespace ActorCastings.Services.Tests
                 ImageUrl = "test",
                 IsApproved = true
             },
-            new Movie()
+            new Play()
             {
-                Id = Guid.Parse("c538936c-83e0-4a8a-a3ed-a957d362d59e"),
-                Title = "New Movie2",
-                Genre = "Comedy",
+                Id = Guid.Parse("f70c5c2c-967a-405b-88ef-25782bf796f3"),
+                Title = "New Play2",
+                Theatre = "Public",
                 Director = "John George",
-                Description = "Test description of the new comedy movie",
+                Description = "Test description of the new comedy play",
                 ReleaseYear = 2022,
                 ImageUrl = "test",
                 IsApproved = true
@@ -67,62 +68,62 @@ namespace ActorCastings.Services.Tests
         [SetUp]
         public void Setup()
         {
-            _movieRepository = new Mock<IRepository<Movie, Guid>>();
+            _playRepository = new Mock<IRepository<Play, Guid>>();
             _actorRepository = new Mock<IRepository<Actor, Guid>>();
-            _actorMovieRepository = new Mock<IRepository<ActorMovie, Guid>>();
+            _actorPlayRepository = new Mock<IRepository<ActorPlay, Guid>>();
 
-            IQueryable<Movie> movieMockQueryable = movies.BuildMock();
+            IQueryable<Play> playMockQueryable = plays.BuildMock();
 
-            _movieRepository
+            _playRepository
                 .Setup(m => m.GetAllAttached())
-                .Returns(movieMockQueryable);
+                .Returns(playMockQueryable);
 
-            movieService = new MovieService(_movieRepository.Object, _actorRepository.Object, _actorMovieRepository.Object);
+            playService = new PlayService(_playRepository.Object, _actorRepository.Object, _actorPlayRepository.Object);
 
         }
 
         [Test]
         [TestCase(1, 6, 2)]
         [TestCase(2, 1, 1)]
-        public async Task GetAllPaginatedMoviesPositive(int page, int pageSize, int expectedCount)
+        public async Task GetAllPaginatedPlaysPositive(int page, int pageSize, int expectedCount)
         {
 
-            IEnumerable<MovieViewModel> allMoviesActual = await movieService
-                .IndexGetPaginatedMoviesAsync(page, pageSize);
+            IEnumerable<PlayViewModel> allPlaysActual = await playService
+                .IndexGetPaginatedPlaysAsync(page, pageSize);
 
-            Assert.That(allMoviesActual, Is.Not.Null);
-            Assert.That(allMoviesActual.Count(), Is.EqualTo(expectedCount));
+            Assert.That(allPlaysActual, Is.Not.Null);
+            Assert.That(allPlaysActual.Count(), Is.EqualTo(expectedCount));
         }
 
         [Test]
         [TestCase(-1, 6)]
         [TestCase(2, 0)]
-        public async Task GetAllPaginatedMoviesNegative(int page, int pageSize)
+        public async Task GetAllPaginatedPlaysNegative(int page, int pageSize)
         {
 
             Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () =>
             {
-                IEnumerable<MovieViewModel> allMoviesActual = await movieService
-                .IndexGetPaginatedMoviesAsync(page, pageSize);
+                IEnumerable<PlayViewModel> allPlaysActual = await playService
+                .IndexGetPaginatedPlaysAsync(page, pageSize);
 
             });
         }
 
         [Test]
-        public async Task GetMoviesCountPositive()
+        public async Task GetPlaysCountPositive()
         {
-            int actual = await movieService
-                .GetMoviesCountAsync();
+            int actual = await playService
+                .GetPlaysCountAsync();
 
             Assert.That(actual, Is.Not.Zero);
-            Assert.That(actual, Is.EqualTo(movies.Count));
+            Assert.That(actual, Is.EqualTo(plays.Count));
         }
 
         [Test]
-        [TestCase("395413df-fe4d-4caf-8875-c3dc050a44ed", 1)]
-        public async Task GetMovieDetailsPositive(string id, int collectionCount)
+        [TestCase("2c459674-8238-4ea8-8fe4-21f0bf5acb16", 1)]
+        public async Task GetPlayDetailsPositive(string id, int collectionCount)
         {
-            MovieDetailsViewModel actual = await movieService.GetMovieDetailsAsync(id);
+            PlayDetailsViewModel actual = await playService.GetPlayDetailsAsync(id);
 
             Assert.That(actual, Is.Not.Null);
             Assert.That(actual.Actors.Count, Is.EqualTo(collectionCount));
@@ -131,26 +132,26 @@ namespace ActorCastings.Services.Tests
         [Test]
         [TestCase("wrongid")]
         [TestCase("")]
-        public async Task GetMovieDetailsNegativeInvalidId(string id)
+        public async Task GetPlayDetailsNegativeInvalidId(string id)
         {
             Assert.ThrowsAsync<ArgumentException>(async () =>
             {
-                MovieDetailsViewModel actual = await movieService.GetMovieDetailsAsync(id);
+                PlayDetailsViewModel actual = await playService.GetPlayDetailsAsync(id);
             });
         }
 
         [Test]
         [TestCase("e2c67618-1a8d-47da-8778-a9df40503b56")]
-        public async Task GetActorDetailsNegativeNotFoundActor(string id)
+        public async Task GetPlayDetailsNegativeNotFoundActor(string id)
         {
             Assert.ThrowsAsync<KeyNotFoundException>(async () =>
             {
-                MovieDetailsViewModel actual = await movieService.GetMovieDetailsAsync(id);
+                PlayDetailsViewModel actual = await playService.GetPlayDetailsAsync(id);
             });
         }
 
         [Test]
-        public async Task AddMovieAndRoleInItPositive()
+        public async Task AddPlayAndRoleInItPositive()
         {
             var userId = Guid.NewGuid().ToString();
             var guidUserId = Guid.Parse(userId);
@@ -161,10 +162,10 @@ namespace ActorCastings.Services.Tests
                 UserId = guidUserId
             };
 
-            var model = new AddMovieViewModel
+            var model = new AddPlayViewModel
             {
                 Title = "Test Movie",
-                Genre = "Action",
+                Theatre = "Test Theatre",
                 Description = "Test Description",
                 Director = "Test Director",
                 ImageUrl = "http://testimage.com",
@@ -176,19 +177,19 @@ namespace ActorCastings.Services.Tests
                 .Setup(a => a.FirstOrDefaultAsync(It.IsAny<Expression<Func<Actor, bool>>>()))
                 .ReturnsAsync(mockActor);
 
-            _movieRepository
-                .Setup(m => m.AddAsync(It.IsAny<Movie>()))
+            _playRepository
+                .Setup(p => p.AddAsync(It.IsAny<Play>()))
                 .Returns(Task.CompletedTask);
 
-            _actorMovieRepository
-                .Setup(am => am.AddAsync(It.IsAny<ActorMovie>()))
+            _actorPlayRepository
+                .Setup(am => am.AddAsync(It.IsAny<ActorPlay>()))
                 .Returns(Task.CompletedTask);
 
-            await movieService.AddMovieAndRoleInItAsync(model, userId);
+            await playService.AddPlayAndRoleInItAsync(model, userId);
 
-            _movieRepository.Verify(m => m.AddAsync(It.Is<Movie>(m =>
+            _playRepository.Verify(m => m.AddAsync(It.Is<Play>(m =>
                 m.Title == model.Title &&
-                m.Genre == model.Genre &&
+                m.Theatre == model.Theatre &&
                 m.Description == model.Description &&
                 m.Director == model.Director &&
                 m.ImageUrl == model.ImageUrl &&
@@ -196,22 +197,22 @@ namespace ActorCastings.Services.Tests
                 m.IsApproved == false
             )), Times.Once);
 
-            _actorMovieRepository.Verify(repo => repo.AddAsync(It.Is<ActorMovie>(am =>
-                am.ActorId == mockActor.Id &&
-                am.Role == model.Role &&
-                am.IsApproved == false
+            _actorPlayRepository.Verify(repo => repo.AddAsync(It.Is<ActorPlay>(ap =>
+                ap.ActorId == mockActor.Id &&
+                ap.Role == model.Role &&
+                ap.IsApproved == false
             )), Times.Once);
         }
 
         [Test]
         [TestCase("ff")]
         [TestCase("")]
-        public async Task AddMovieAndRoleInItNegativeIvalidId(string id)
+        public async Task AddPlayAndRoleInItNegativeIvalidId(string id)
         {
-            var model = new AddMovieViewModel
+            var model = new AddPlayViewModel
             {
                 Title = "Test Movie",
-                Genre = "Action",
+                Theatre = "Test Theatre",
                 Description = "Test Description",
                 Director = "Test Director",
                 ImageUrl = "http://testimage.com",
@@ -221,18 +222,18 @@ namespace ActorCastings.Services.Tests
 
             Assert.ThrowsAsync<ArgumentException>(async () =>
             {
-                await movieService.AddMovieAndRoleInItAsync(model, id);
+                await playService.AddPlayAndRoleInItAsync(model, id);
             });
         }
 
         [Test]
         [TestCase("0ccd5e52-d989-4b59-b83c-a1b57a7f6cf8")]
-        public async Task AddMovieAndRoleInItNegativeInvalidUser(string id)
+        public async Task AddPlayAndRoleInItNegativeInvalidUser(string id)
         {
-            var model = new AddMovieViewModel
+            var model = new AddPlayViewModel
             {
                 Title = "Test Movie",
-                Genre = "Action",
+                Theatre = "Test Theatre",
                 Description = "Test Description",
                 Director = "Test Director",
                 ImageUrl = "http://testimage.com",
@@ -242,7 +243,7 @@ namespace ActorCastings.Services.Tests
 
             Assert.ThrowsAsync<Exception>(async () =>
             {
-                await movieService.AddMovieAndRoleInItAsync(model, id);
+                await playService.AddPlayAndRoleInItAsync(model, id);
             });
         }
     }
